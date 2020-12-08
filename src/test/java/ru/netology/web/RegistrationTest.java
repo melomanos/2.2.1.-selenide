9a8250d3f3a37bc3fa56ad575a5df44f0d6e6158
melomanos <philippov_mihail@mail.ru>
2020-12-08 18:40:19 +0300

    Reworked. All tasks are done!

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,48 +1,81 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
+    LocalDate date = LocalDate.now();
+    int dayOfMonth = date.plusDays(3).getDayOfMonth();
+    int month = date.getMonthValue();
+    int year = date.getYear();
+    String dayOfMonthStr = String.valueOf(dayOfMonth);
+    String yearStr = String.valueOf(year);
+    String monthStr = String.valueOf(month);
+    String validDate = dayOfMonthStr + monthStr + yearStr;
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldRegisterSuccessful() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Василий Петров");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 13000);
+        $(withText(dayOfMonthStr + "." + monthStr + "." + yearStr)).waitUntil(visible, 13000);
     }
 
     @Test
     void shouldChooseCityFromDropDownList() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Мо");
         $$(By.className("menu-item__control")).find(exactText("Смоленск")).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Василий Петров");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 13000);
+        $(withText(dayOfMonthStr + "." + monthStr + "." + yearStr)).waitUntil(visible, 13000);
+    }
+
+    @Test
+    void shouldDateFromCalendarApp() {
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $(By.className("icon_name_calendar")).click();
+        $("[data-step='1']").doubleClick();
+        $$(By.className("calendar__day")).find(exactText("16")).click();
+        $("[data-test-id='name'] input").setValue("Василий Петров");
+        $("[data-test-id='phone'] input").setValue("+79200000000");
+        $("[data-test-id='agreement']").click();
+        $$("[type='button']").find(exactText("Забронировать")).click();
+        $(withText("16.02.2021")).waitUntil(visible, 13000);
     }
 
     @Test
     void shouldRegisterAllFieldsEmpty() {
-        open("http://localhost:9999");
         $$("[type='button']").find(exactText("Забронировать")).click();
         $(withText("Поле обязательно для заполнения")).waitUntil(visible, 3000);
     }
 
     @Test
     void shouldRegisterWrongCity() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Chicago");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Василий Петров");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
@@ -52,7 +85,6 @@ class RegistrationTest {
 
     @Test
     void shouldRegisterWrongDate() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
         $("[data-test-id='date'] input").setValue("10.12.2019");
@@ -65,8 +97,9 @@ class RegistrationTest {
 
     @Test
     void shouldRegisterWrongName() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Oleg Ivanov");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
@@ -76,8 +109,9 @@ class RegistrationTest {
 
     @Test
     void shouldRegisterWrongPhone() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Василий Петров");
         $("[data-test-id='phone'] input").setValue("+7920000000");
         $("[data-test-id='agreement']").click();
@@ -87,12 +121,14 @@ class RegistrationTest {
 
     @Test
     void shouldRegisterDoNotPushRadioButton() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
         $("[data-test-id='name'] input").setValue("Василий Петров");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $$("[type='button']").find(exactText("Забронировать")).click();
-        $(withText("Я соглашаюсь с условиями обработки")).waitUntil(visible, 3000);
+        $$("[data-test-id='agreement'].input_invalid .checkbox__text")
+                .find(new Text("Я соглашаюсь с условиями обработки и использования моих персональных данных"))
+                .waitUntil(visible, 3000);
     }
 }
-
